5697c2cf020818f6cf927bfa3a61df8d65d66119
AAA-lanyangyang <119588915+llljinjinjin@users.noreply.github.com>
2023-12-23 21:52:00 +0800

    Update main_dreamer_swin_gamma.py

--- code/dreamer/main_dreamer_swin_gamma.py
+++ code/dreamer/main_dreamer_swin_gamma.py
@@ -52,7 +52,8 @@ def eval_test(model, x, y):
                     count=count+1
             acc = accuracy_score(pred, lab)  # acc
             conMat = confusion_matrix(pred, lab)  # confusion matrix
-            f = f1_score(pred, lab, average=None)[0]  # f1
+            f_ = f1_score(pred, lab, average=None)  # f1
+            f=np.mean(f_,axis=0)
             # kappa
             axis1 = np.sum(conMat, axis=1)
             axis0 = np.sum(conMat, axis=0)
