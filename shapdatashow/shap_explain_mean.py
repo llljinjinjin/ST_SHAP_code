f5db4f4b60c57790b2909eff114dd0e5e74e6117
AAA-777888999 <119588915+surnfnogl@users.noreply.github.com>
2024-11-04 12:00:43 +0800

    Update shap_explain_mean.py

--- shapdatashow/shap_explain_mean.py
+++ shapdatashow/shap_explain_mean.py
@@ -3,7 +3,7 @@ import torch, torchvision
 import shap
 import numpy as np
 from sklearn.model_selection import train_test_split  # Partition data set
-from model.SWIN_trans import SwinTransformer
+from ST_SHAP_code.model.SWIN_trans import SwinTransformer
 import heapq
 # GradientExplainer Explain the model using the expected gradient (extension of the integrated gradient)
 
